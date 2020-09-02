ccc13e5d464223893f5b700afad96c4bd19dc9e7
AoxLiu <64894896+AoxLiu@users.noreply.github.com>
2020-09-02 11:35:17 +0300

    Create 0_SCE_Income_Check.R

---
+++ Finnish/0_SCE_Income_Check.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(ggplot2)
+library(viridis)
+
+r_dir <- "/homes/aliu/DSGE_LRS/input/r_files/"
+
+
+sce_inc <- get(load(pate0(r_dir, "sce_inc.Rdata")))
+edu_high <- get(load(pate0(r_dir, "edu_high.Rdata")))
+
+
+## Some summary information
+
+# N. of rows
+nrow(sce_inc) # 
+
+# N. of individuals
+length(unique(sce_inc$TNRO))
+
+# Classification included
+
+# Statistical years
+unique(sce_inc$vuosi)
+
+# Check year of birth for those with no information - does not explain why for some SES is missing
+mean(sce_inc[sce_inc$sose=="","b_year"])
+
+# Cell with highest income in statistical year 2017
+min(sce_inc[sce_inc$vuosi==2017,"Eur"],na.rm=T)
+max(sce_inc[sce_inc$vuosi==2017,"Eur"],na.rm=T)
+median(sce_inc[sce_inc$vuosi==2017,"Eur"],na.rm=T)
+mean(sce_inc[sce_inc$vuosi==2017,"Eur"],na.rm=T)
+
+# Occyupation with highest median income
+sce_inc %>% group_by(Text) %>% summarise(mean_Eur=median(Eur,na.rm=T)) %>% print(n=100)
+
+# Median education level for each profession
+sce_inc_sorted <- sce_inc %>% arrange(TNRO,-Eur) %>% distinct(TNRO, keep_all=T)
+
+dm <- merge(sce_inc_sorted, edu_high, by="TNRO")
+
+dm %>% group_by(Text) %>% summarise(mean_edu=mean(as.numeric(as.character(EduYears)),na.rm=T)) %>% print(n=100)
+
