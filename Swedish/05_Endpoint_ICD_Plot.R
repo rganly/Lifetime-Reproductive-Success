3074813f12dd26cf7415d17ec343dee55202b80b
AoxLiu <64894896+AoxLiu@users.noreply.github.com>
2020-09-24 17:08:24 +0300

    Update 05_Endpoint_ICD_Plot.R

--- Swedish/05_Endpoint_ICD_Plot.R
+++ Swedish/05_Endpoint_ICD_Plot.R
@@ -1,8 +1,6 @@
 ## This script is to plot prevalence of endpoints from two datasets
 
-
 setwd("/Users/aoxliu/Documents/Project1_Lifetime_Reproductive_Success/Data/FIN_SWE/Plot/")
-
 
 library(ggplot2)
 library(plotly)
@@ -30,8 +28,6 @@ plot_preval <- function(dat, x_var, y_var, x_name, y_name, extra_name){
 				theme_classic()
 	save_html(ggplotly(p, tooltip="text"), paste0("Prevalence_",x_var,"_",y_var,"_",extra_name,".html"))
 }
-
-
 
 
 
@@ -63,6 +59,8 @@ nrow(ep_W_b50)   # 1,438
 plot_preval(dat=ep_W, x_var="Prevalence_IndexW", y_var="Prevalence_FIN_indexWN", x_name="Finnish index person (born 1956-1982)", y_name="Swedish index person (born 1956-1982)", extra_name="EndpointsAll")
 plot_preval(dat=ep_W_f50, x_var="Prevalence_IndexW", y_var="Prevalence_FIN_indexWN", x_name="Finnish index person (born 1956-1982)", y_name="Swedish index person (born 1956-1982)", extra_name="Endpoints50Count_either")
 plot_preval(dat=ep_W_b50, x_var="Prevalence_IndexW", y_var="Prevalence_FIN_indexWN", x_name="Finnish index person (born 1956-1982)", y_name="Swedish index person (born 1956-1982)", extra_name="Endpoints50Count_both")
+
+
 
 
 
